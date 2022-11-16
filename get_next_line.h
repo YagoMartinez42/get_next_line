f1b24951b3a7b224e70f6409a67fe95bd7c6c52a
samartin <samartin@student.42madrid.com>
2022-11-16 17:03:34 +0100

    .h clean

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: samartin <samartin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/04 18:24:26 by samartin          #+#    #+#             */
-/*   Updated: 2022/11/06 16:50:44 by samartin         ###   ########.fr       */
+/*   Updated: 2022/11/16 16:56:28 by samartin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 13
+#  define BUFFER_SIZE 512
 # endif
 # include <stdlib.h>
 # include <unistd.h>
@@ -23,8 +23,5 @@ char	*get_next_line(int fd);
 void	*gnl_mexpand(void *ptr, int size);
 int		gnl_len(const char *str);
 char	*gnl_strncat(char *dest, char *src, int nb);
-
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 
 #endif
